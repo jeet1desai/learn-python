2df354a5a0bdd1b55f528b662fa49ea69d30ac2e
Jeet Desai <jeetdesai995@gmail.com>
2023-11-21 11:35:01 +0530

    learned basic list in py

---
+++ Day2/lists.py
@@ -0,0 +1,14 @@
+number_list = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 9 ]
+print(number_list[0])
+
+# Add
+number_list.append(10)
+print(number_list)
+
+# Remove
+number_list.remove(9)
+print(number_list)
+
+# Length
+print(len(number_list))
+ 
